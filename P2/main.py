4be3a834ada5ba61d5579091f59d1a4b126436e8
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:06:36 +0800

    feat: add find_highest_gwa method using min()

--- P2/main.py
+++ P2/main.py
@@ -9,3 +9,16 @@ class StudentGWA:
         with open(self.filename, 'r') as file:
             data = [line.strip().split() for line in file.readlines()]
         return data
+class StudentGWA:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_data(self):
+        with open(self.filename, 'r') as file:
+            data = [line.strip().split() for line in file.readlines()]
+        return data
+
+    def find_highest_gwa(self):
+        data = self.read_data()
+        top_student = min(data, key=lambda x: float(x[1]))
+        return top_student

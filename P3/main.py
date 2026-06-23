2b6a0900d10192958eafd88d2d42e71b2cc95030
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:15:42 +0800

    feat: add find_min_max method

--- P3/main.py
+++ P3/main.py
@@ -9,3 +9,17 @@ class NumberStats:
         with open(self.filename, 'r') as file:
             numbers = [int(num) for num in file.read().split()]
         return numbers
+class NumberStats:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_numbers(self):
+        with open(self.filename, 'r') as file:
+            numbers = [int(num) for num in file.read().split()]
+        return numbers
+
+    def find_min_max(self):
+        numbers = self.read_numbers()
+        smallest = min(numbers)
+        largest = max(numbers)
+        return smallest, largest

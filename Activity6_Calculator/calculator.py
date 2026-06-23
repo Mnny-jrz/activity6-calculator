710d7b9ecf724728fd9eac0d472cb91b1e709c30
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-24 01:21:30 +0800

    add Operation base class

---
+++ Activity6_Calculator/calculator.py
@@ -0,0 +1,16 @@
+class Operation:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            print("⚠️ Cannot divide by zero.")
+            return None
